78d977b993cdc061704392ba30c05d915d07ac94
ImGabreuw <imgabreuw.dev@gmail.com>
2023-09-16 23:01:13 -0300

    polynomial standardization

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include "utils.h"
 
 int main(int argc, char const *argv[])
 {
+    char *polynomial = standardization("4x^6 + 3x^5 - 6x^3 - 8x^2 + 3x + 4");
+
+    int index = 0;
+    while (polynomial[index] != '\0')
+    {
+        printf("%c", polynomial[index]);
+        index++;
+    }
     
+    printf("\n");
+
     return 0;
 }
 
--- utils.h
+++ utils.h
@@ -4,13 +4,54 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#define SIZE_EACH_TERM 6
-#define MAX_SIZE (SIZE_EACH_TERM * 10)
+#define MAX_SIZE 1000
+
+void removeSpaces(char *str);
+
+void polynomialInput(void);
+
+int getHighestDegree(char polynomial[]);
+
+int countSpaces(char str[]);
+
+char *standardization(char polynomial[]);
+
+void removeSpaces(char *str);
 
 void polynomialInput(void)
 {
   char polynomial[MAX_SIZE];
   fgets(polynomial, MAX_SIZE, stdin);
+}
+
+int getHighestDegree(char polynomial[])
+{
+  char highestDegree[20];
+
+  for (int i = 0; i < strlen(polynomial); i++)
+  {
+    if (polynomial[i] != '^')
+      continue;
+
+    int degreeStartIndex = i + 1;
+    int degreeLastIndex = 0;
+
+    for (int j = degreeStartIndex; j < strlen(polynomial); j++)
+    {
+      if (!isspace(polynomial[j]))
+        continue;
+
+      degreeLastIndex = j;
+      break;
+    }
+
+    strncpy(highestDegree, polynomial + degreeStartIndex, degreeLastIndex - degreeStartIndex);
+    highestDegree[degreeLastIndex - degreeStartIndex] = '\0';
+
+    return atoi(highestDegree);
+  }
+
+  return 0;
 }
 
 int countSpaces(char str[])
@@ -28,28 +69,70 @@ int countSpaces(char str[])
   return count;
 }
 
-char *split(char str[], char delimiters[])
+char *standardization(char polynomial[])
 {
-  int size = SIZE_EACH_TERM * countSpaces(str);
-  char *splitted = (char *)malloc(size * sizeof(char));
-  
-  bool toSplit = true;
+  int index = 0;
+  char *stdPolynomial = (char *)malloc(2 * MAX_SIZE * sizeof(char));
+  int currentPolynomialDegree = getHighestDegree(polynomial);
 
-  for (int i = 0; i < strlen(str); i++)
+  bool toSplit = true;
+  int startIndex = -1;
+
+  for (int i = 0; i <= strlen(polynomial); i++)
   {
-    if (!isspace(str[i]))
+    if (startIndex == -1)
+    {
+      startIndex = i;
+    }
+
+    if (!isspace(polynomial[i]) && polynomial[i] != '\0')
       continue;
 
     if (toSplit)
     {
+      char temp[20];
+      strncpy(temp, polynomial + startIndex, i - startIndex);
+      temp[i - startIndex] = '\0';
+      removeSpaces(temp);
 
+      if (getHighestDegree(temp) != currentPolynomialDegree)
+      {
+        for (int j = getHighestDegree(temp) + 1; j <= currentPolynomialDegree; j++)
+        {
+          char tempBuffer[20];
+          snprintf(tempBuffer, sizeof(tempBuffer), "+0x^%d", j);
+          strcat(stdPolynomial, tempBuffer);
+          --currentPolynomialDegree;
+        }
+      }
+
+      strcat(stdPolynomial, temp);
+      --currentPolynomialDegree;
 
       toSplit = false;
+      startIndex = -1;
       continue;
     }
 
     toSplit = true;
   }
 
-  return 
+  return stdPolynomial;
+}
+
+void removeSpaces(char *str)
+{
+  int len = strlen(str);
+  int i, j = 0;
+
+  for (i = 0; i < len; i++)
+  {
+    if (str[i] != ' ')
+    {
+      str[j] = str[i];
+      j++;
+    }
+  }
+
+  str[j] = '\0';
 }
