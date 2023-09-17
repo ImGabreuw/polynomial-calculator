94ab9595119f24b1bd2b722d22809912d6d33fa1
ImGabreuw <imgabreuw.dev@gmail.com>
2023-09-17 16:14:38 -0300

    refactor: clean code

--- polynomial.h
+++ polynomial.h
@@ -9,45 +9,45 @@ PolynomialTerm polynomial1[MAX_SIZE_TERM];
 
 PolynomialTerm polynomial2[MAX_SIZE_TERM];
 
-void transform(char polynomial1[], char polynomial2[]);
+void transform(PolynomialTerm pt[], char polynomial[]);
 
-void transform(char polynomial1[], char polynomial2[])
+void transform(PolynomialTerm pt[], char polynomial[])
 {
   int startIndex = 0;
 
-  for (int i = 0; i <= strlen(polynomial1); i++)
+  for (int i = 0; i <= strlen(polynomial); i++)
   {
-    if (polynomial1[i] == '+' || polynomial1[i] == '-')
+    if (polynomial[i] == '+' || polynomial[i] == '-')
     {
       startIndex = i;
     }
 
-    if (polynomial1[i] == 'x')
+    if (polynomial[i] == 'x')
     {
       int endIndex = i - 1;
       char tempCoef[20];
       char tempDegree[20];
-      strncpy(tempCoef, polynomial1 + startIndex, endIndex - startIndex);
+      strncpy(tempCoef, polynomial + startIndex, endIndex - startIndex);
       tempCoef[endIndex - startIndex] = '\0';
 
-      for (int j = i + 1; j <= strlen(polynomial1); j++)
+      for (int j = i + 1; j <= strlen(polynomial); j++)
       {
-        if (polynomial1[j] == '^')
+        if (polynomial[j] == '^')
         {
           startIndex = j + 1;
           continue;
         }
 
-        if (polynomial1[j] == '+' || polynomial1[j] == '-' || polynomial1[j] == '\0')
+        if (polynomial[j] == '+' || polynomial[j] == '-' || polynomial[j] == '\0')
         {
           endIndex = j - 1;
           printf("%d %d\n", startIndex, endIndex);
-          strncpy(tempDegree, polynomial1 + startIndex, endIndex - startIndex);
+          strncpy(tempDegree, polynomial + startIndex, endIndex - startIndex);
           tempDegree[endIndex - startIndex] = '\0';
           break;
         }
       }
-      
+
       int coef = atoi(tempCoef);
       int degree = atoi(tempDegree);
 
