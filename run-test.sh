a2e42bcb5c686a4560d815f5c729fe5443c93591
ImGabreuw <imgabreuw.dev@gmail.com>
2023-09-20 13:41:19 -0300

    styles: add messages in test script

--- run-test.sh
+++ run-test.sh
@@ -3,6 +3,7 @@
 gcc -o main main.c polynomial-term.c polynomial.c utils.c -lm
 
 # Casos de testes para resolver o polinômio
+echo "---------------------------- Resolver polinômio ----------------------------"
 ./main 1 11 2 "2x^2 + x + 1"
 ./main 1 5 1 "x^2 + 2x + 2"
 ./main 1 134 3 "3x^3 + 4x^2 + 5x + 2"
@@ -15,4 +16,5 @@ gcc -o main main.c polynomial-term.c polynomial.c utils.c -lm
 ./main 1 1 -2 "-2x^3 - 3x^2 + 2x + 1"
 
 # Casos de teste para somar polinômios
+echo "---------------------------- Somar polinômios ----------------------------"
 ./main 2 "2x^2 + 2x^1 + 2" "x^2 + x^1 + 1" "x^2 + x^1 + 1"
